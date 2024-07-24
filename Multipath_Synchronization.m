f4408e93c8d107880c310b413d3883a007be941a
barbaalba <39873583+barbaalba@users.noreply.github.com>
2024-07-24 15:35:35 +0200

    Chirp Signal
    Changed transmitted signal from Sinc to Chirp

--- Multipath_Synchronization.m
+++ Multipath_Synchronization.m
@@ -1,22 +1,25 @@
-clc;clear;close all;
+clc; clear; close all;
+
 % Parameters
 fs = 1e6; % Sampling frequency (Hz)
 signal_duration = 10e-3; % Duration of the signal in seconds (longer the signal is, the more accurate the estimation)
 t = 0:1/fs:signal_duration; % Time vector for the signal duration
 signal_length = length(t); % Length of the signal
 
-% Transmitted Signal (sinc function)
-tx_signal = sinc(2*pi*5000*(t-signal_duration/2));
+% Transmitted Signal (chirp function)
+f0 = 1000; % Start frequency of the chirp
+f1 = 50000; % End frequency of the chirp
+tx_signal = chirp(t, f0, signal_duration, f1);
 
 % Simulate the received signal with delay
-multipath_gain = [1, 0.5 0.3];
+multipath_gain = [1, 0.5, 0.3];
 multipath_delays = [50e-6, 150e-6, 300e-6]; % Delays in seconds
 delay_samples = round(multipath_delays * fs); % Delay in samples
 
-delayed_copies = repmat(tx_signal,length(multipath_gain),1);
+delayed_copies = repmat(tx_signal, length(multipath_gain), 1);
 for i = 1:length(multipath_gain)
-    delayed_copies(i,:) = circshift(delayed_copies(i,:),delay_samples(i));
-    delayed_copies(i,1:delay_samples(i)) = 0;
+    delayed_copies(i, :) = circshift(delayed_copies(i, :), delay_samples(i));
+    delayed_copies(i, 1:delay_samples(i)) = 0;
 end
 
 rx_signal = sum(multipath_gain' .* delayed_copies);
@@ -29,7 +32,7 @@ rx_signal_noisy = awgn(rx_signal, SNR, 'measured');
 [corr, lags] = xcorr(rx_signal_noisy, tx_signal);
 
 % Find the peaks of the cross-correlation
-[pks, locs] = findpeaks(corr, 'MinPeakHeight', max(corr)*0.5);
+[pks, locs] = findpeaks(corr, 'MinPeakHeight', max(corr) * 0.5);
 
 % Select the highest peak
 [~, max_idx] = max(pks);
@@ -38,20 +41,20 @@ estimated_delay = estimated_delay_samples / fs;
 
 % Plotting
 figure;
-subplot(3,1,1);
+subplot(3, 1, 1);
 plot(t, tx_signal);
 title('Transmitted Signal');
 xlabel('Time (s)');
 ylabel('Amplitude');
 
-subplot(3,1,2);
+subplot(3, 1, 2);
 plot(t, rx_signal_noisy);
 title('Received Signal with Noise');
 xlabel('Time (s)');
 ylabel('Amplitude');
 
-subplot(3,1,3);
-plot(lags/fs, corr);
+subplot(3, 1, 3);
+plot(lags / fs, corr);
 title('Cross-Correlation');
 xlabel('Lag (s)');
 ylabel('Correlation');
