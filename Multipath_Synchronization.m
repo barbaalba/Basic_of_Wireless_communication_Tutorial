4a25064c490f6f4900a228039ae3e6566d5a3a66
barbaalba <39873583+barbaalba@users.noreply.github.com>
2024-07-24 15:49:21 +0200

    Fixed figure

--- Multipath_Synchronization.m
+++ Multipath_Synchronization.m
@@ -11,9 +11,23 @@ f0 = 1000; % Start frequency of the chirp
 f1 = 50000; % End frequency of the chirp
 tx_signal = chirp(t, f0, signal_duration, f1);
 
+figure;
+subplot(2, 1, 1);
+plot(t, tx_signal);
+title('Transmitted Signal');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+subplot(2, 1, 2);
+plot(fs/signal_length*(-signal_length/2:signal_length/2-1), abs(fftshift(fft(tx_signal))));
+xlim([-10e4,10e4])
+title('Frequency Response');
+xlabel('Frequency (Hz)');
+ylabel('Amplitude');
+
 % Simulate the received signal with delay
 multipath_gain = [1, 0.5, 0.3];
-multipath_delays = [50e-6, 150e-6, 300e-6]; % Delays in seconds
+multipath_delays = [50e-6, 80e-6, 120e-6]; % Delays in seconds
 delay_samples = round(multipath_delays * fs); % Delay in samples
 
 delayed_copies = repmat(tx_signal, length(multipath_gain), 1);
@@ -41,19 +55,13 @@ estimated_delay = estimated_delay_samples / fs;
 
 % Plotting
 figure;
-subplot(3, 1, 1);
-plot(t, tx_signal);
-title('Transmitted Signal');
-xlabel('Time (s)');
-ylabel('Amplitude');
-
-subplot(3, 1, 2);
+subplot(2, 1, 1);
 plot(t, rx_signal_noisy);
 title('Received Signal with Noise');
 xlabel('Time (s)');
 ylabel('Amplitude');
 
-subplot(3, 1, 3);
+subplot(2, 1, 2);
 plot(lags / fs, corr);
 title('Cross-Correlation');
 xlabel('Lag (s)');
