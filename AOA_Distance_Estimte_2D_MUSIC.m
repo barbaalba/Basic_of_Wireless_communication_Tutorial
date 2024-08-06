30437b3d42cc3f0cadb2aa1280ecdfeab1beca4f
barbaalba <39873583+barbaalba@users.noreply.github.com>
2024-08-06 14:34:02 +0200

    Location Estimation Done!

--- AOA_Distance_Estimte_2D_MUSIC.m
+++ AOA_Distance_Estimte_2D_MUSIC.m
@@ -4,7 +4,7 @@ clear; clc; close all;
 fc = 28e9; % carrier frequency in Hz
 c = 3e8; % speed of light
 lambda = c/fc; % wavelength propagating waves
-M = 50; % number of antennas ULA
+M = 256; % number of antennas ULA
 d = 1/2; % normalized inter-element spacing
 D = sqrt(lambda^2/4 + (M*lambda*d)^2); % diagonal length of array aperture
 d_FA = 2*D^2/lambda; % Fraunhofer distance for an array in meter
@@ -13,14 +13,15 @@ d_NF = d_FA/10; % the upper threshold to be in near-field region
 d_bjo = 2*D; % the lower threshold to be in near-field region with constant amplitude
 disp(['Lower bound distance: ', num2str(d_bjo)]);
 disp(['Upperbount distance: ', num2str(d_NF)]);
-SNR = db2pow(0); % SNR
+SNR = db2pow(20); % SNR
 L = 100; % pilot length
 k = 1; % number of users
 
 % generate a user at random location in near-field of an array
-r_k = unifrnd(d_F,d_FA,1,k);
+r_k = unifrnd(d_bjo,d_NF,1,k);
 varphi = unifrnd(0,pi,1,k);
 disp(['User is located at distance ', num2str(r_k), ' from the array']);
+disp(['User is located at angular direction of ', num2str(varphi)]);
 % Evaluate precise user location
 x_k = r_k.*cos(varphi);
 y_k = r_k.*sin(varphi);
@@ -47,7 +48,7 @@ U_n = U(:,1:(M-k)); % noise sub_space
 
 % Exhaustive search over azimuth and distance 
 angle_search = 0:pi/180:pi;
-distance_search = d_F:9*d_F:d_FA;
+distance_search = linspace(d_bjo,d_NF,1000);
 MUSIC_spectrum = zeros(length(angle_search),length(distance_search));
 for l = 1:length(distance_search)
     grid_response = ArrayResponse(angle_search,distance_search(l));
@@ -58,3 +59,14 @@ end
 surfc(X,Y,MUSIC_spectrum,'EdgeColor','none');
 xlabel('Distance [m]','FontSize',20,'Interpreter','latex');
 ylabel('AoA [rad]','FontSize',20,'Interpreter','latex');
+
+[~,idx] = max(MUSIC_spectrum,[],"all");
+[row,col] = ind2sub([length(angle_search),length(distance_search)],idx);
+disp(['Estimted AoA: ',num2str(angle_search(row))]);
+disp(['Estimated distance: ',num2str(distance_search(col))]);
+
+x_est = distance_search(col) .* cos(angle_search(row));
+y_est = distance_search(col) .* sin(angle_search(row));
+
+loc_error = sqrt((x_est - x_k).^2 + (y_est-y_k).^2);
+disp(['Localization error: ',num2str(loc_error),' [m]']);
